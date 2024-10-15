a9569cc22aceb3fd9c6af8e0bcd66511360d51cc
Sha Fin <99030043+shshafin@users.noreply.github.com>
2024-10-16 00:52:16 +0600

    Build and fix issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import Routes from "./Routes/Routes.jsx";
-import AuthProvider from "./Provider/AuthProvider.jsx";
+import Routes from "./Routes/Routes";
+import AuthProvider from "./Provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
